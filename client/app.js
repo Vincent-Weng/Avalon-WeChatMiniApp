e8e7a1dfea48522272f7c287d311c6dd20972c07
Vincent <wenghchn@outlook.com>
2018-04-21 10:51:20 -0400

    Update files to the new source

--- client/app.js
+++ client/app.js
@@ -1,9 +1,9 @@
 //app.js
-var qcloud = require('./vendor/wafer2-client-sdk/index')
-var config = require('./config')
-
 App({
-    onLaunch: function () {
-        qcloud.setLoginUrl(config.service.loginUrl)
+    config: {
+        host: 'mp.techird.cn'
+    },
+    onLaunch () {
+        console.log('App.onLaunch()');
     }
-})
+});
